23ad8bfd93ba6cfddef5d846f7ce41cb80fa1a3d
Marcelo-MC505 <marcelo.molieri11916@est.uca.edu.ni>
2022-04-26 18:43:56 -0600

    Primeros cambios de navegacion

---
+++ app/src/main/java/com/rsamqui/bakingbills/UI/ScreenSplashActivity.kt
@@ -0,0 +1,35 @@
+package com.rsamqui.bakingbills.UI
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.rsamqui.bakingbills.MainActivity
+import com.rsamqui.bakingbills.R
+import com.rsamqui.bakingbills.databinding.ActivityScreenSplashBinding
+
+class ScreenSplashActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityScreenSplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityScreenSplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        startSplashScreen()
+
+    }
+
+    private fun startSplashScreen(timeDelayInMilis : Long = 5000) {
+        Handler(Looper.myLooper()!!).postDelayed(
+            {
+                startFirstActivity()
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                finish()
+            }, timeDelayInMilis
+        )
+    }
+
+    private fun startFirstActivity() = startActivity(Intent(this,MainActivity::class.java))
+
+
+}
