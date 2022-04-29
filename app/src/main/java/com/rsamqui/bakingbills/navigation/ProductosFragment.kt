3452f0a5e69030b299218c611268b4502f466b95
Rodolfo Samqui <rsamqui501@gmail.com>
2022-04-28 22:20:09 -0600

    HomeFragment creado

---
+++ app/src/main/java/com/rsamqui/bakingbills/navigation/ProductosFragment.kt
@@ -0,0 +1,17 @@
+package com.rsamqui.bakingbills.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ProductosFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+}
